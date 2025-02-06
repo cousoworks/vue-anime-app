03b7c30933fa4b71f72368c5d5c46b4b49d09f81
Blayneraptor <cousoworks@gmail.com>
2025-02-06 16:27:14 +0100

    Añadiendo transparencia card

--- src/App.vue
+++ src/App.vue
@@ -174,7 +174,7 @@ h1 {
 
 .anime-card {
   width: 300px;
-  background-color: rgba(255, 255, 255, 0.7); /* Fondo semi-transparente */
+  background-color: rgba(255, 255, 255, 0.678); /* Fondo semi-transparente */
   border-radius: 10px;
   box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
   overflow: hidden;
