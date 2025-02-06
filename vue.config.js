79b0b8d6c18641802dc633c9a1791578b9999607
Blayneraptor <cousoworks@gmail.com>
2025-02-06 16:41:39 +0100

    Cambiando vue config

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,5 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-anime-app/'
-    : '/'
+  publicPath: './' // Esto evita problemas de rutas en GitHub Pages
 });
